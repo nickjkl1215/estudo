ba5f0bab69656a307fba921246ca5db3f72f1403
nickjkl1215 <italonew15@outlook.com>
2021-05-06 01:08:07 -0300

    novo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <Primeiro curso="Fluencia em 10 dias" :detalhes="true" /> -->
+  <div v-for="(cidade,i) in cidades" :key="i">
+      <Primeiro :curso="cidade.name" :detalhes="cidade.exibirDetalhes"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Primeiro from "./components/Primeiro";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Primeiro,
+  },
+  data() {
+    return {
+      cidades: [
+        { name: "Salvador", exibirDetalhes: false },
+        { name: "Roma", exibirDetalhes: true },
+        { name: "Kiev", exibirDetalhes: false },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
 #app {
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+* {
+  margin: 0;
 }
 </style>
